f88fddf32a9dd548a57a1e24861ba6bf0129bcba
habiba <habibasangarimohammed@gmail.com>
2023-04-21 06:33:38 -0700

    hello

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This unit defines a text file-reading function"""
+
+
+def read_file(filename=""):
+    """displays the contents of a UTF8 text file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
+
