62ad1c06d777ba7ed3cfd03029532b88307cd0fb
habiba <habibasangarimohammed@gmail.com>
2023-04-27 15:51:26 -0700

    hello

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -31,4 +31,3 @@ void print_python_string(PyObject *p)
 	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
 	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
 	printf("  value: %s\n", PyBytes_AsString(str));
-}
