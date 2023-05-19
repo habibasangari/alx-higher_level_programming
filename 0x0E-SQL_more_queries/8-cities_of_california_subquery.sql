c783b9ff1f57687026ec734ea25f2b147e28b541
habiba <habibasangarimohammed@gmail.com>
2023-05-18 18:58:18 -0700

    hello

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- script to lists all cities of califonia in the database hbtn_0d_usa.
+-- Results are ordered by ascending cities.id.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
