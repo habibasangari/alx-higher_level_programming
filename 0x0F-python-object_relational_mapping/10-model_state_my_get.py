2b727a361310e457d39a44e672efa174be295d4f
habiba <habibasangarimohammed@gmail.com>
2023-06-24 14:49:16 -0700

    hello

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
 """
-displays the State object with the name passed as argument from the db
+script to print the State object id
+with the name passed as argument
+from the database `hbtn_0e_6_usa`.
 """
-import sys
+
+
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
-    session = Session()
 
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-    print("Not found" if not state else state.id)
+    session = Session()
+    instance = session.query(State).filter(State.name == argv[4]).first()
+
+    if instance is None:
+        print('Not found')
+    else:
+        print('{0}'.format(instance.id))
