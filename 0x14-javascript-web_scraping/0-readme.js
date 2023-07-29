74df22f60440331f989cbea9179f553deee769dd
habibasangari <habibasangarimohammed@gmail.com>
2023-07-29 03:57:48 -0700

    hello

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,6 @@
 
 
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf-8', (error, data) => {
-  if (error) { console.log(error); } else { console.log(data); }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
