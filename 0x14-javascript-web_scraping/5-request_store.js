cf555aa80bd49677ddfb787293569b5c0e5616eb
habibasangari <habibasangarimohammed@gmail.com>
2023-07-29 07:13:58 -0700

    hello

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-
-
 const fs = require('fs');
-
 const request = require('request');
-const fileStream = fs.createWriteStream(process.argv[3], 'utf-8');
-
-request.get(process.argv[2]).pipe(fileStream);
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
