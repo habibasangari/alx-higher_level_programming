8aec3314c22ebcd38b78f2cb68e4b0023a578f14
habibasangari <habibasangarimohammed@gmail.com>
2023-07-29 07:07:00 -0700

    hello

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-
-
 const request = require('request');
-
-
-request.get(process.argv[2], (error, response, body) => {
-  if (error) { console.log(error); }
-  if (response) { console.log('code:', response.statusCode); }
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
